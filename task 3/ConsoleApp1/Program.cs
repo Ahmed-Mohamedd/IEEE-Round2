b53fbcc15e7546f7b110c7a8cc687156615e3436
Ahmed Mohamed <aahmedfarhatt@yahoo.com>
2022-03-21 19:24:31 +0200

    first commit

---
+++ task 3/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter size of array");
+            int n = int.Parse(Console.ReadLine());
+            int [] array= new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("enter element number " + i);
+                array[i] = int.Parse(Console.ReadLine());   
+            }
+            Console.WriteLine("the second largest number in array is " + gettingSecondLargestElement(array));
+            Console.ReadLine();
+        }
+
+        private static int gettingSecondLargestElement(int [] array)
+        {
+            int n = array.Length;
+            int temp = 0;
+            for(int i = 0; i<array.Length; i++)   //1,2,8,5,6,9
+            {
+                for (int inner = 0; inner < array.Length-i-1; inner++)
+                {
+                    if (array[inner] > array[inner+1])
+                    {
+                        temp = array[inner];
+                        array[inner] = array[inner+1];
+                        array[inner+1] = temp;
+                    }
+                }
+            }
+            for(int i = n-1; i >= 0; i--)
+            {
+                if(array[i] != array[i-1])
+                {
+                    return array[i-1];
+                }
+            }
+            return 0;
+        }
+    }
+
+
+}
---
+++ task 4/ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            Console.WriteLine("enter a word ");
+            word = Console.ReadLine();
+            findingNonRepeatingChars(word.ToLower());      
+            Console.ReadLine();
+        }
+
+        private static void findingNonRepeatingChars(string word)
+        {
+            ArrayList result = new ArrayList();
+
+            foreach (char c in word)
+            {
+                if (result.IndexOf(c) == -1)
+                {
+                    result.Add(c);
+                }
+                else
+                {
+                    result.RemoveAt(result.IndexOf(c));
+                }
+
+            }
+            Console.WriteLine("the first non-repeating char is  " + result[0]); // first non-repeating char 
+
+            for (int i = 1; i < result.Count; i++) // the rest of non repeating chars if found
+            {
+                Console.WriteLine("the other non-repeating chars is  " + result[i]);
+            }
+
+        }
+    }
+}
